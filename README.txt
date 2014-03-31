85cfc86bd7757cb353ba2c20c2e29812187efa56
daviddiliberto <ddilibertonyc@gmail.com>
2014-03-31 19:04:45 +0000

    first commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/daviddiliberto/pen/yiEwb.
+
+ Latest tweets from Gekko. A bitcoin trading bot on BTC-E.
---
+++ js/twitter.js
@@ -0,0 +1,31 @@
+/**
+ * How to use fetch function:
+ * @param {string} Your Twitter widget ID.
+ * @param {string} The ID of the DOM element you want to write results to. 
+ * @param {int} Optional - the maximum number of tweets you want returned. Must
+ *     be a number between 1 and 20.
+ * @param {boolean} Optional - set true if you want urls and hash
+       tags to be hyperlinked!
+ * @param {boolean} Optional - Set false if you dont want user photo /
+ *     name for tweet to show.
+ * @param {boolean} Optional - Set false if you dont want time of tweet
+ *     to show.
+ * @param {function/string} Optional - A function you can specify to format
+ *     tweet date/time however you like. This function takes a JavaScript date
+ *     as a parameter and returns a String representation of that date.
+ *     Alternatively you may specify the string 'default' to leave it with
+ *     Twitter's default renderings.
+ * @param {boolean} Optional - Show retweets or not. Set false to not show.
+ * @param {function/string} Optional - A function to call when data is ready. It
+ *     also passes the data to this function should you wish to manipulate it
+ *     yourself before outputting. If you specify this parameter you  must
+ *     output data yourself!
+ * @param {boolean} Optional - Show links for reply, retweet, favourite. Set false to not show.
+ */
+
+twitterFetcher.fetch('449662231056117760', 'tweets', 5, true, true, true, dateFormatter, false, null, false);
+
+function dateFormatter(date) {
+  // hack to get time zones to match CST  :(
+  return moment(date).subtract('h', 6).fromNow();
+}
